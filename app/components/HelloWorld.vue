5220ad2ef660474d668f8d19d828375eef41ea23
singsing215 <felvex@163.com>
2019-11-24 23:47:08 +0800

    delete unnecessary data

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -137,38 +137,6 @@
         data() {
             return {
                 chartData: [],
-                categoricalSource: [{
-                        Country: "Germany",
-                        Amount: 15,
-                        SecondVal: 14,
-                        ThirdVal: 24
-                    },
-                    {
-                        Country: "France",
-                        Amount: 13,
-                        SecondVal: 23,
-                        ThirdVal: 25
-                    },
-                    {
-                        Country: "Bulgaria",
-                        Amount: 24,
-                        SecondVal: 17,
-                        ThirdVal: 23
-                    },
-                    {
-                        Country: "Spain",
-                        Amount: 11,
-                        SecondVal: 19,
-                        ThirdVal: 24
-                    },
-                    {
-                        Country: "USA",
-                        Amount: 18,
-                        SecondVal: 8,
-                        ThirdVal: 21
-                    }
-                ],
-
                 inCart: [],
                 total: 0,
                 ladies: [],
